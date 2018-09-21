d396ce98d1201f906bbeca9e33c0a50dd2ffe9b8
Elias <eliasbenur@hotmail.com>
2018-09-21 12:46:34 +0200

    edit

--- Assets/TutoNiv1.cs
+++ Assets/TutoNiv1.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public class TutoNiv1 : MonoBehaviour {
 
     private bool active = true;
-    private bool canSkip = false;
 
 	// Use this for initialization
 	void Start () {
         Time.timeScale = 0;
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -18,7 +17,6 @@ public class TutoNiv1 : MonoBehaviour {
         if (active && Input.GetKeyUp(KeyCode.Joystick1Button0))
         {
             gameObject.SetActive(false);
-            active = false;
             Time.timeScale = 1;
         }
 
