236a57a2473426987a391410337ec9abae32151f
Elias <eliasbenur@hotmail.com>
2018-09-21 08:27:47 +0200

    Player Selection

--- Assets/scripts/PlayerManager.cs
+++ Assets/scripts/PlayerManager.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 namespace Manager
 {
     public class PlayerManager : MonoBehaviour
@@ -12,6 +14,16 @@ namespace Manager
         private IDictionary<Player, Transform> PlayerTransformList;
 
         private static bool created = false;
+
+
+
+        //PJ Selection
+        public Sprite[] CharacterList; //5
+        public GameObject[] PicutreList; // 4
+        public bool[] character_selected; // 5
+        public float f_delay = 0;
+        public int[] pj_selected; // Taille 4, valeurs (0-->4)
+
         void Awake()
         {
             if (!created)
@@ -35,12 +47,112 @@ namespace Manager
                 GameObject playerObject = Instantiate(PlayerPrefabs, SpawnPositions[player.GetId() - 1].position, Quaternion.identity, PlayerFolder);
                 playerObject.GetComponent<SpriteRenderer>().color = player.GetColor();
             }*/
+
+            for (int x = 0; x < character_selected.Length; x++)
+            {
+                character_selected[x] = false;
+            }
         }
 
         // Update is called once per frame
         void Update()
         {
+            if (f_delay < 1.5f)
+            {
+                f_delay += 0.1f;
+            }
+            else
+            {
+                if (Input.GetAxis("DPad_XAxis_1") != 0)
+                {
+                    if (Input.GetAxis("DPad_XAxis_1") < 0)
+                    {
+                        character_selected[pj_selected[0]] = false;
+                        pj_selected[0]--;
+                        if (pj_selected[0] < 0)
+                        {
+                            pj_selected[0] = 4;
+                        }
+                        while (character_selected[pj_selected[0]])
+                        {
+                            pj_selected[0]--;
+                            if (pj_selected[0] < 0)
+                            {
+                                pj_selected[0] = 4;
+                            }
+                        }
+                        character_selected[pj_selected[0]] = true;
+                        PicutreList[0].GetComponent<Image>().sprite = CharacterList[pj_selected[0]];
 
+                    }
+                    else
+                    {
+                        character_selected[pj_selected[0]] = false;
+                        pj_selected[0]++;
+                        if (pj_selected[0] > 4)
+                        {
+                            pj_selected[0] = 0;
+                        }
+                        while (character_selected[pj_selected[0]])
+                        {
+                            pj_selected[0]++;
+                            if (pj_selected[0] > 4)
+                            {
+                                pj_selected[0] = 0;
+                            }
+                        }
+                        character_selected[pj_selected[0]] = true;
+                        PicutreList[0].GetComponent<Image>().sprite = CharacterList[pj_selected[0]];
+                    }
+                    f_delay = 0;
+
+                }
+
+                if (Input.GetAxis("DPad_YAxis_1") != 0)
+                {
+                    if (Input.GetAxis("DPad_YAxis_1") < 0)
+                    {
+                        character_selected[pj_selected[1]] = false;
+                        pj_selected[1]--;
+                        if (pj_selected[1] < 0)
+                        {
+                            pj_selected[1] = 4;
+                        }
+                        while (character_selected[pj_selected[1]])
+                        {
+                            pj_selected[1]--;
+                            if (pj_selected[1] < 0)
+                            {
+                                pj_selected[1] = 4;
+                            }
+                        }
+                        character_selected[pj_selected[1]] = true;
+                        PicutreList[1].GetComponent<Image>().sprite = CharacterList[pj_selected[1]];
+
+                    }
+                    else
+                    {
+                        character_selected[pj_selected[1]] = false;
+                        pj_selected[1]++;
+                        if (pj_selected[1] > 4)
+                        {
+                            pj_selected[1] = 0;
+                        }
+                        while (character_selected[pj_selected[1]])
+                        {
+                            pj_selected[1]++;
+                            if (pj_selected[1] > 4)
+                            {
+                                pj_selected[1] = 0;
+                            }
+                        }
+                        character_selected[pj_selected[1]] = true;
+                        PicutreList[1].GetComponent<Image>().sprite = CharacterList[pj_selected[1]];
+                    }
+                    f_delay = 0;
+
+                }
+            }
         }
 
         public bool IsPlayerAlreadyInLobby(int controllerId)
@@ -60,13 +172,26 @@ namespace Manager
             //Player NewPlayer = new Player(id, PlayerColorList[PlayerList.Count], ControllerId);
             PlayerList.Add(player);
             Debug.Log("Controller id:" + ControllerId + " added as Player n°" + id);
+
+            for (int x = 0; x < character_selected.Length; x++)
+            {
+                if (!character_selected[x])
+                {
+                    character_selected[x] = true;
+                    pj_selected[id - 1] = x;
+                    PicutreList[id - 1].GetComponent<Image>().sprite = CharacterList[pj_selected[id - 1]];
+                    break;
+                }
+            }
+
             return id;
         }
 
         public int GetPlayerByControllerId(int controllerId)
         {
             //while(Player)
-            if (PlayerList.Exists(item => item.ControllerId == controllerId)){
+            if (PlayerList.Exists(item => item.ControllerId == controllerId))
+            {
                 Player player = PlayerList.Find(item => item.ControllerId == controllerId);
                 return player.id;
             }
